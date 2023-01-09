50159c249364c3ce5e8c3aff3750b0eca7da5412
pizza-narie <114802632+pizza-narie@users.noreply.github.com>
2023-01-09 12:50:43 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,12 @@ class User < ApplicationRecord
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
 
-  validates :nickname, presence: true
-  validates :birth_day, presence: true
-
+  
   with_options presence: true do
 
-    validates :encrypted_password, format: { with: PASSWORD_REGEX, message:'には英字と数字の両方を含めて設定してください' }, length: { minimum: 6 }, confirmation: true 
+    validates :nickname
+    validates :birth_day
+    validates :password, format: { with: PASSWORD_REGEX, message:'には英字と数字の両方を含めて設定してください' }, confirmation: true 
     validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: '@を含めて入力してください' }, uniqueness: { case_sensitive: false } 
     validates :family_name, :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
     validates :family_name_kana, :first_name_kana, format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters."}
