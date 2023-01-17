07fd765d461d4403f821d174ef374ebf6cd28037
pizza-narie <114802632+pizza-narie@users.noreply.github.com>
2023-01-17 23:38:41 +0900

    テストコード実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,19 @@
 FactoryBot.define do
   factory :item do
-    
+    name              {'善逸の刀'}
+    description       {'本物です。プレミアが付く前にどうぞ'}
+    price             {'112800'}
+    condition_id      {3}
+    cost_id           {2}
+    scheduled_day_id  {2}
+    prefecture_id     {2}
+    category_id       {3}
+
+    user
+
+    after(:build) do |message|
+      message.image.attach(io: File.open('public/test_images/a.jpg'), filename: 'a.jpg')
+    end
+
   end
 end
