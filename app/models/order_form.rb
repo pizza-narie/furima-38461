6b0b3f76f7337f6f5cc593db1f1bdeed9542c7a1
pizza-narie <114802632+pizza-narie@users.noreply.github.com>
2023-01-29 13:36:55 +0900

    購入情報データ保存機能の実装

---
+++ app/models/order_form.rb
@@ -0,0 +1,21 @@
+class OrderForm
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :city, :address1, :address2, :telephone, :token
+
+  with_options presence: true do
+    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: " Enter it as follows (e.g. 123-4567)"}
+    validates :city
+    validates :address1
+    validates :telephone, format: { with: /\A\d{,11}\z/, message: '11桁以内の数字を入力してください' }
+    validates :prefecture_id, numericality: { other_than: 1, message: 'Select'}
+    validates :token
+    validates :user_id
+    validates :item_id
+  end
+
+   def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    order.create_address(post_code: post_code, prefecture_id: prefecture_id, city: city, address1: address1, address2: address2, 
+                   telephone: telephone)
+  end
+end
