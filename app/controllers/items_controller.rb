dd9da148ab2a36db1d65475ab49f65000032a292
pizza-narie <114802632+pizza-narie@users.noreply.github.com>
2023-01-29 13:37:54 +0900

    購入状況による画面の表示切り替え機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -51,7 +51,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    if current_user != @item.user
+    if current_user != @item.user || @item.order.present?
       redirect_to root_path 
     end
   end
